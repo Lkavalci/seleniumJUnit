7cde42181163f401ec8a54a0fa4ad8fd4213dccc
lutfi <lutfikavalci@gmail.com>
2023-05-14 07:44:49 +0300

    230514first

--- src/test/java/day10_actionClass_Faker_FileTestleri/C05_FileDownloadTest.java
+++ src/test/java/day10_actionClass_Faker_FileTestleri/C05_FileDownloadTest.java
@@ -1,40 +1,38 @@
 package day10_actionClass_Faker_FileTestleri;
 
-import utilities.TestBase;
-
-public class C05_FileDownloadTest extends TestBase {
-
-
-}
-/*
-package day10_actionsClass_Faker_FileTestleri;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import utilities.TestBase;
+
 import java.nio.file.Files;
 import java.nio.file.Paths;
+
 public class C05_FileDownloadTest extends TestBase {
+
+
     @Test
     public void test01() throws InterruptedException {
         //2. https://the-internet.herokuapp.com/download adresine gidelim.
         driver.get("https://the-internet.herokuapp.com/download");
-        //3. foto.png dosyasını indirelim
-        WebElement fotoElementi= driver.findElement(By.linkText("foto.png"));
+        //3. image.jpg dosyasını indirelim
+        WebElement fotoElementi= driver.findElement(By.xpath("//*[@id='content']/div/a[7]"));
         fotoElementi.click();
-        Thread.sleep(3000);
+        Thread.sleep(5000);
+
+
         //4. Dosyanın başarıyla indirilip indirilmediğini test edelim
-        // Dosyayi Downloads klasorune indirecek
-        String dosyaYolu= "/Users/ahmetbulutluoz/Downloads/foto.png";
+        // Dosyayi Downloads (karşıdan yüklenenler) klasorune indirecek
+        String dosyaYolu="C:/Users/Hilal Nur/Downloads/image.jpg";
         Assert.assertTrue(Files.exists(Paths.get(dosyaYolu)));
         // herkesin bilgisayarinda farkli bir dosya hiyerarsisi oldugundan
         // herkesin dosya yolu birbirinden farkli olur
         // Dosya yolunu dinamik hale getirmek icin Java'dan yardim alacagiz
         //C:\Users\emre.cigit\Downloads\foto.png"
-        //"C:\Users\Berke\Downloads\foto.png"
+        //"C:\Users\Berke\Downloads\foto.png" ..gibi...
         System.out.println(System.getProperty("user.dir"));
         System.out.println(System.getProperty("user.home"));
+
     }
 }
- */
--- src/test/java/day10_actionClass_Faker_FileTestleri/C06_DosyaYolunuDinamikYapma.java
+++ src/test/java/day10_actionClass_Faker_FileTestleri/C06_DosyaYolunuDinamikYapma.java
@@ -1,23 +1,11 @@
 package day10_actionClass_Faker_FileTestleri;
 
-import org.junit.Test;
-
-public class C06_DosyaYolunuDinamikYapma {
-    @Test
-    public void test01(){
-
-        System.out.println(System.getProperty("user.dir"));
-// C:\Users\Hilal Nur\IdeaProjects\com.seleniumJUnit
-        System.out.println(System.getProperty("user.home"));
-       // C:\Users\Hilal Nur
-    }
-}
-/*
-package day10_actionsClass_Faker_FileTestleri;
 import org.junit.Assert;
 import org.junit.Test;
+
 import java.nio.file.Files;
 import java.nio.file.Paths;
+
 public class C06_DosyaYolunuDinamikYapma {
     @Test
     public void test01(){
@@ -29,25 +17,25 @@ public class C06_DosyaYolunuDinamikYapma {
         //"C:\Users\Berke\Downloads\foto.png"
         System.out.println(System.getProperty("user.dir"));
         // projenin dosya yolunu verir
-        // /Users/ahmetbulutluoz/IdeaProjects/com.Team113JUnit
+// C:\Users\Hilal Nur\IdeaProjects\com.seleniumJUnit
         System.out.println(System.getProperty("user.home"));
-        ///Users/ahmetbulutluoz
-        /* biz Downloads'a indirdigim dosyanin dosya yolunu 2'ye bolecegiz
-            /Users/ahmetbulutluoz               /Downloads/foto.png
+       // C:\Users\Hilal Nur
+
+         /* biz Downloads'a indirdigim dosyanin dosya yolunu 2'ye bolecegiz
+            /Users/ahmetbulutluoz               /Downloads/image.jpg
             herkesin bilgisayarinda farkli      herkeste ayni
-            System.getProperty("user.home")     /Downloads/foto.png
+            System.getProperty("user.home")     /Downloads/image.jpg
          */
-    /*
-String dosyaYoluDegisenKisim = System.getProperty("user.home");
-    String dosyaYoluherkesteAyniOlanKisim = "/Downloads/foto.png";
-    String dosyaYolu= dosyaYoluDegisenKisim + dosyaYoluherkesteAyniOlanKisim;
+        String dosyaYoluDegisenKisim = System.getProperty("user.home");
+        String dosyaYoluherkesteAyniOlanKisim = "/Downloads/image.jpg";
+        String dosyaYolu= dosyaYoluDegisenKisim + dosyaYoluherkesteAyniOlanKisim;
         Assert.assertTrue(Files.exists(Paths.get(dosyaYolu)));
-                // ayni islemi projemizin altindaki deneme.txt icin yapsak
-                // src/test/java/day10_actionsClass_Faker_FileTestleri/deneme.txt
-                dosyaYoluDegisenKisim = System.getProperty("user.dir");
-                dosyaYoluherkesteAyniOlanKisim = "/src/test/java/day10_actionsClass_Faker_FileTestleri/deneme.txt";
-                dosyaYolu = dosyaYoluDegisenKisim + dosyaYoluherkesteAyniOlanKisim;
-                Assert.assertTrue(Files.exists(Paths.get(dosyaYolu)));
-                }
-                }
- */
+        // ayni islemi projemizin altindaki deneme.txt icin yapsak
+        // src/test/java/day10_actionsClass_Faker_FileTestleri/deneme.txt
+        dosyaYoluDegisenKisim = System.getProperty("user.dir");
+        dosyaYoluherkesteAyniOlanKisim = "src/test/java/day10_actionClass_Faker_FileTestleri/deneme.txt";
+        dosyaYolu = dosyaYoluDegisenKisim + dosyaYoluherkesteAyniOlanKisim;
+        Assert.assertTrue(Files.exists(Paths.get(dosyaYolu)));
+
+    }
+}
