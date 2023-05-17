18ac20e439fedd241ee866376ecacdcc3786e103
lutfi <lutfikavalci@gmail.com>
2023-05-17 16:21:38 +0300

    230517first

--- src/test/java/day12_webTables_excelOtomasyon/C02_WebTables.java
+++ src/test/java/day12_webTables_excelOtomasyon/C02_WebTables.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebElement;
 import org.w3c.dom.stylesheets.LinkStyle;
 import utilities.TestBase;
 
+import java.util.List;
+
 public class C02_WebTables extends TestBase {
 
 
@@ -19,69 +21,43 @@ public class C02_WebTables extends TestBase {
         driver.get("https://demoqa.com/webtables");
 
    //     2. Headers da bulunan basliklari yazdirin
-        //List<WebElement> basliklarList=driver.findElements(By.xpath("//div[@role='columnheader']"));
-       // for (WebElement eachHeader:basliklarList) {
-        //    System.out.println(eachHeader.getText());
-      //  }
+        List<WebElement> basliklarList=driver.findElements(By.xpath("//div[@role='columnheader']"));
+        for (WebElement eachHeader:basliklarList) {
+           System.out.println(eachHeader.getText());
+        }
    //     3. 3.sutunun basligini yazdirin
+        System.out.println("3.sutun baslıgı : "+basliklarList.get(3).getText());
 
    //     4. Tablodaki tum datalari yazdirin
-   //     5. Tabloda kac tane bos olmayan cell (data) oldugunu yazdirin
-   //     6. Tablodaki satir sayisini yazdirin
-   //     7. Tablodaki sutun sayisini yazdirin
-    //    8. Tablodaki 3.kolonu yazdirin
-    //    9. Tabloda "First Name" i Kierra olan kisinin Salary'sini yazdirin
-    //    10. Page sayfasinda bir method olusturun, Test sayfasindan satir ve sutun sayisini girdigimde bana datayi yazdirsin
-
-    }
-}
-/*
-package day12_webTables_excelOtomasyon;
-import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import utilities.TestBase;
-import java.util.List;
-public class C02_WebTables extends TestBase {
-    @Test
-    public void test01(){
-        //  1. “https://demoqa.com/webtables” sayfasina gidin
-        driver.get("https://demoqa.com/webtables");
-        //  2. Headers da bulunan basliklari yazdirin
-        List<WebElement> basliklarList = driver.findElements(By.xpath("//div[@role='columnheader']"));
-        for (WebElement eachHeader: basliklarList
-             ) {
-            System.out.println(eachHeader.getText());
-        }
-        //  3. 3.sutunun basligini yazdirin
-        System.out.println("3.sutun basligi : " + basliklarList.get(3).getText());
-        //  4. Tablodaki tum datalari yazdirin
-        System.out.println("===================");
         int bosOlmayanDataSayisi=0;
-        List<WebElement> tabloDatalari = driver.findElements(By.xpath("//div[@role='gridcell']"));
-        for (WebElement eachData:tabloDatalari
+        List<WebElement> tablodatalari=driver.findElements(By.xpath("//div[@role='gridcell']"));
+        for (WebElement eachData:tablodatalari
              ) {
             if (!eachData.getText().isBlank()){
                 System.out.println(eachData.getText());
                 bosOlmayanDataSayisi++;
             }
         }
-        //  5. Tabloda kac tane bos olmayan cell (data) oldugunu yazdirin
+   //     5. Tabloda kac tane bos olmayan cell (data) oldugunu yazdirin
         System.out.println("Tabloda bos olmayan hucre sayisi : " + bosOlmayanDataSayisi);
-        //  6. Tablodaki satir sayisini yazdirin
+
+   //     6. Tablodaki satir sayisini yazdirin
         List<WebElement> satirlarList = driver.findElements(By.xpath("//div[@role='rowgroup']"));
-        System.out.println("Tablodaki satir sayisi : " + satirlarList.size());
-        //  7. Tablodaki sutun sayisini yazdirin
-        System.out.println("tablodaki sutun sayisi : " + basliklarList.size());
-        //  8. Tablodaki 3.kolonu yazdirin
-        List<WebElement> ucuncuSutunElementleri = driver.findElements(By.xpath("(//div[@role='row'])/div[3]"));
-        System.out.println("======3.sutun=======");
+        System.out.println("Tablodaki satir sayisi :"+satirlarList.size());
+
+   //     7. Tablodaki sutun sayisini yazdirin
+        System.out.println("Tablodaki sutun sayisi: "+basliklarList.size());
+
+    //    8. Tablodaki 3.kolonu yazdirin
+        List<WebElement> ucuncuSutunElementleri= driver.findElements(By.xpath("(//div[@role='row'])/div[3]"));
+        System.out.println("00000000000000 3. SUTUN 0000000000000000");
         for (WebElement eachData:ucuncuSutunElementleri
-        ) {
+             ) {
             if (!eachData.getText().isBlank()){
                 System.out.println(eachData.getText());
             }
         }
+
         //  9. Tabloda "First Name" i Kierra olan kisinin Salary'sini yazdirin
         for (int i = 2; i < satirlarList.size() ; i++) {
             WebElement isim = driver.findElement(By.xpath(getDinamikXpath(i,1)));
@@ -106,4 +82,3 @@ public class C02_WebTables extends TestBase {
         return "(//div[@role='row'])["+satirNo+"]/div["+sutunNo+"]" ;
     }
 }
- */
